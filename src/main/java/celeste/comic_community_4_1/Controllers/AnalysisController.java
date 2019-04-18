1bf77a3a7c98a47accb231c3ed5918ce08e41a89
Yifan Wang <yifanwang07@163.com>
2019-04-17 20:30:56 -0400

    Profile Links

--- src/main/java/celeste/comic_community_4_1/Controllers/AnalysisController.java
+++ src/main/java/celeste/comic_community_4_1/Controllers/AnalysisController.java
@@ -2,8 +2,10 @@ package celeste.comic_community_4_1.Controllers;
 
 import celeste.comic_community_4_1.exception.ResourceNotFoundException;
 import celeste.comic_community_4_1.miscellaneous.AnalysisData;
+import celeste.comic_community_4_1.model.Follow;
 import celeste.comic_community_4_1.model.User;
 import celeste.comic_community_4_1.repository.AnalysisRepository;
+import celeste.comic_community_4_1.repository.FollowRepository;
 import celeste.comic_community_4_1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class AnalysisController {
@@ -18,6 +21,9 @@ public class AnalysisController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    FollowRepository followRepository;
 
     @Autowired
     AnalysisRepository analysisRepository;
@@ -29,9 +35,9 @@ public class AnalysisController {
         }
         // Find Current User
         String username = (String) request.getSession().getAttribute("username");
-        User founduser = userRepository.findById(username)
+        User user = userRepository.findById(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
-        model.addAttribute("User", founduser);
+        model.addAttribute("User", user);
 
         // Analysis data
         AnalysisData analysisData = new AnalysisData();
@@ -49,6 +55,13 @@ public class AnalysisController {
         analysisData.setSubscriber(new long[4]);
 
         model.addAttribute("analysisData", analysisData);
+
+        //Get Follows
+        List<Follow> c = followRepository.findByFollowIndentityUserone(user);
+        model.addAttribute("following", c.size());
+        //Get Followers
+        List<Follow> d = followRepository.findByFollowIndentityUsertwo(user);
+        model.addAttribute("followers", d.size());
         return "/profile_analysis";
     }
 
