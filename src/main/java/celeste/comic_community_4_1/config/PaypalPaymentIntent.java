e03966480d02a31334e9a19956462ac4bf88d02a
DESKTOP-84SKG63\Frost <yingnan.li@stonybrook.edu>
2019-05-13 14:13:00 -0400

    +Paypal checkout
    Paypal checkout button is available.
    - There is something wrong on redirection. (when pay is success)

---
+++ src/main/java/celeste/comic_community_4_1/config/PaypalPaymentIntent.java
@@ -0,0 +1,7 @@
+package celeste.comic_community_4_1.config;
+
+public enum PaypalPaymentIntent {
+
+	sale, authorize, order
+	
+}
---
+++ src/main/java/celeste/comic_community_4_1/config/PaypalPaymentMethod.java
@@ -0,0 +1,7 @@
+package celeste.comic_community_4_1.config;
+
+public enum PaypalPaymentMethod {
+
+	credit_card, paypal
+	
+}
